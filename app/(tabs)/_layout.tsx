fa482a9ff644101b63f688805429b301330415f2
EqualsOS <osequals@gmail.com>
2025-08-27 15:53:20 +1000

    2025-08-27 Setup the triforce library book for web.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,9 +34,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="library"
         options={{
-          title: 'Explore',
+          title: 'Library',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
